708d7daec45b624267c0469334ea5f508aaf9078
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2018-04-21 07:40:02 +0200

    --++

--- BackEnd/models/student.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var StudentSchema = new mongoose.Schema({
-    name: String,
-    address: String,
-    phones: [{name: String, address: String}]
-});
-
-module.exports = mongoose.model('Student', StudentSchema);
--- BackEnd/models/subject.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var subjectSchema = new mongoose.Schema({
-    name: String,
-    students: [{type: mongoose.Schema.Types.ObjectId, ref:'Student' }]
-});
-
-module.exports = mongoose.model('Subject', subjectSchema);
